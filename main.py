78d91b90ca1fc4c22f0b51dbae15c3232e263fc9
Mirror83 <glenochieng045@gmail.com>
2023-10-02 21:50:14 +0300

    Added a rectangle to represent the spring

--- main.py
+++ main.py
@@ -7,14 +7,25 @@ from stack import Stack
 
 pygame.init()
 
-screen = pygame.display.set_mode(size=(1000, 600))
+SCREEN_WIDTH, SCREEN_HEIGHT = 1200, 800
+
+screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Stack Visualization")
 
 candy_group = CandyGroup()
 candy_stack = Stack()
 
+spring_rectangle_height = 300
+Candy.VERTICAL_START = SCREEN_HEIGHT - spring_rectangle_height - 120
+
+# The rectangle here is given an arbitrary left and right since
+# the midbottom property (assigned on the next line) will take care of its positioning
+spring_rectangle = pygame.rect.Rect((10, 10), (200, spring_rectangle_height))
+spring_rectangle.midbottom = (600, 700)
+
 clock = pygame.time.Clock()
 MAX_FPS = 60
+
 
 while True:
     for event in pygame.event.get():
@@ -23,23 +34,28 @@ while True:
             exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            candy = Candy()
-            candy_stack.push(candy)
-            candy_group.add(candy)
-            Candy.decrement_vertical_start()
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.button == 1:
+                candy = Candy()
+                candy_stack.push(candy)
+                candy_group.add(candy)
+                candy_group.shift_after_addition()
+                Candy.decrement_vertical_start()
+                spring_rectangle.inflate_ip(0, -10)
+                spring_rectangle.midbottom = (600, 700)
+            elif event.button == 3:
                 if not candy_stack.is_empty():
                     candy_group.remove(candy_stack.pop())
-                    candy_group.shift_after_removal()
                     Candy.increment_vertical_start()
+                    candy_group.shift_after_removal()
+                    spring_rectangle.inflate_ip(0, 10)
+                    spring_rectangle.midbottom = (600, 700)
                 else:
                     print("Nothing to remove")
 
     screen.fill("Black")  # Erase the screen so that trails of objects at previous positions are not seen
     candy_group.draw(screen)
     candy_group.update()
+    pygame.draw.rect(surface=screen, color="Blue", rect=spring_rectangle, width=spring_rectangle.width)
 
     pygame.display.update()
     clock.tick(MAX_FPS)
