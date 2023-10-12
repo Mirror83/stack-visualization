80b0e8dfd06641d5b64771f5aab6a5aa8504918b
Mirror83 <glenochieng045@gmail.com>
2023-10-12 06:36:35 +0300

    Added EmptyStackException

--- stack.py
+++ stack.py
@@ -2,6 +2,10 @@ from typing import Any
 
 
 class Stack:
+    class EmptyStackException(Exception):
+        def __init__(self):
+            super().__init__("Stack is empty")
+
     def __init__(self):
         self.elements = []
 
@@ -15,7 +19,11 @@ class Stack:
         self.elements.append(element)
 
     def pop(self) -> Any:
+        if self.is_empty():
+            raise Stack.EmptyStackException()
         return self.elements.pop()
 
     def peek(self) -> Any:
-        print(self.elements[len(self) - 1])
+        if self.is_empty():
+            raise Stack.EmptyStackException()
+        return self.elements[len(self) - 1]
