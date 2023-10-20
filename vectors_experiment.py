80578d4be7e4e1d8d2e1a0fcccca72a96382636d
Mirror83 <glenochieng045@gmail.com>
2023-10-20 16:30:35 +0300

    Experimented with sine waves

--- vectors_experiment.py
+++ vectors_experiment.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pygame as pg
 from pygame.math import Vector2
 
@@ -11,39 +12,39 @@ clock = pg.time.Clock()
 SCREEN_SIZE = Vector2(1000, 800)
 screen = pg.display.set_mode(SCREEN_SIZE)
 
-start = Vector2(100, 100)
-stop = Vector2(500, 500)
+multiplier = 1
+start = 100
 
 
-PLAYER_SIZE = Vector2(50, 50)
+def sin_wave(period_multiplier: float, start_y: int) -> list[Vector2]:
+    x: np.ndarray = np.linspace(0, 4 * np.pi, 100)
+    vectors = [Vector2(400 + 20 * np.sin(period_multiplier * num), pg.math.clamp(start_y + 20 * num, start_y, 360)) for
+               num in x if
+               start_y + 20 * num <= 360]
+    vectors.append(Vector2(vectors[len(vectors) - 1].x - 30, vectors[len(vectors) - 1].y))
+    return vectors
 
-player = pg.Rect(start, PLAYER_SIZE)
+
 t = 0
-
-
-def move_player():
-    global player, start, stop, t
-    t += 0.005
-    t = pg.math.clamp(t, 0, 1)
-
-    player.topleft = start.lerp(stop, t)
-
-    if player.topleft == stop:
-        t = 0
-        start, stop = stop, start
-
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             sys.exit()
 
-    screen.fill("White")
-    pg.draw.circle(screen, "Red", start, 10)
-    pg.draw.circle(screen, "Blue", stop, 10)
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == pg.BUTTON_LEFT:
+                multiplier += 0.5
+                start += 10
 
-    move_player()
-    pg.draw.rect(screen, "Pink", player)
+            elif event.button == pg.BUTTON_RIGHT:
+                if multiplier > 1:
+                    multiplier -= 0.5
+                    start -= 10
+
+    screen.fill("White")
+
+    pg.draw.lines(screen, "Black", False, sin_wave(multiplier, start))
 
     pg.display.update()
     clock.tick(MAX_FPS)
