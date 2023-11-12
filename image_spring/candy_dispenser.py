87d8e01e506eed5e973dd5f37122020bcd8b6c0a
Mirror83 <glenochieng045@gmail.com>
2023-11-12 12:03:23 +0300

    Updated CandyDispenser methods to return strings to be output to the screen

--- image_spring/candy_dispenser.py
+++ image_spring/candy_dispenser.py
@@ -15,21 +15,21 @@ class Spring:
         self.base = mid_bottom
         self.rect.midbottom = self.base
 
-        self.resize_factor = 0.88
-        self.resize_exponent = 0
+        self.height_change = 30
+
+    def set_height_change(self, height_change: float):
+        self.height_change = height_change
 
     def reset_rect(self):
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.base
 
     def shrink(self):
-        self.resize_exponent += 1
-        self.image = pg.transform.scale_by(self.original_image, (1, self.resize_factor ** self.resize_exponent))
+        self.image = pg.transform.scale(self.original_image, Vector2(self.rect.size[0], self.rect.size[1] - 30))
         self.reset_rect()
 
     def grow(self):
-        self.resize_exponent -= 1
-        self.image = pg.transform.scale_by(self.original_image, (1, self.resize_factor ** self.resize_exponent))
+        self.image = pg.transform.scale(self.original_image, Vector2(self.rect.size[0], self.rect.size[1] + 30))
         self.reset_rect()
 
     def render(self, surface: Surface):
@@ -40,8 +40,8 @@ class Candy:
     CANDY_COLORS = ["Red", "Green", "Pink", "Yellow"]
     CURRENT_COLOR_INDEX = 0
 
-    def __init__(self, mid_bottom: Vector2):
-        self.size = Vector2(200, 30)
+    def __init__(self, mid_bottom: Vector2, size: Vector2 = Vector2(200, 30)):
+        self.size = size
         self.surface = Surface(self.size).convert_alpha()
         self.surface.fill("Limegreen")
         self.surface.set_colorkey("Limegreen")
@@ -58,7 +58,7 @@ class Candy:
         self.font_rect.center = self.color_rect.center
 
     def __str__(self):
-        return f"Candy({self.number=}, {self.color=})"
+        return f"Candy(number={self.number}, color={self.color})"
 
     def __repr__(self):
         return f"Candy({self.number=}, {self.color=})"
@@ -74,32 +74,47 @@ class Candy:
 
 
 class CandyDispenser:
-    def __init__(self, spring_mid_bottom: Vector2):
-        self.spring = Spring(spring_mid_bottom)
-        self.candies = Stack[Candy]()
+    def __init__(self, mid_bottom: Vector2, candy_size: Vector2 | None = None):
+        self.spring = Spring(Vector2(mid_bottom.x, mid_bottom.y - 10))
+        self.container_rect = Rect(Vector2(0, 0), Vector2(250, self.spring.rect.height + 20))
+        self.container_rect.midbottom = Vector2(mid_bottom)
 
-    def push_candy(self):
-        self.spring.shrink()
-        if self.candies.is_empty():
-            self.candies.push(Candy(self.spring.rect.midtop))
+        if candy_size:
+            self.candy_size = candy_size
         else:
-            previous_candy = self.candies.peek()
-            self.candies.push(Candy(previous_candy.rect.midtop))
-            for i in range(len(self.candies)):
-                if i == 0:
-                    self.candies[i].rect.midbottom = self.spring.rect.midtop
-                else:
-                    self.candies[i].rect.midbottom = self.candies[i - 1].rect.midtop
+            self.candy_size = Vector2(self.container_rect.width - 30, 30)
 
-        Candy.CURRENT_COLOR_INDEX += 1
+        self.spring.set_height_change(self.candy_size.y)
 
-        print(self.candies)
+        self.candies = Stack[Candy]()
+        self.max_candies = 11
 
-    def pop_candy(self):
+    def push_candy(self) -> str:
+        if len(self.candies) < self.max_candies:
+            self.spring.shrink()
+            if self.candies.is_empty():
+                self.candies.push(Candy(self.spring.rect.midtop, self.candy_size))
+            else:
+                previous_candy = self.candies.peek()
+                self.candies.push(Candy(previous_candy.rect.midtop, self.candy_size))
+                for i in range(len(self.candies)):
+                    if i == 0:
+                        self.candies[i].rect.midbottom = self.spring.rect.midtop
+                    else:
+                        self.candies[i].rect.midbottom = self.candies[i - 1].rect.midtop
+
+            Candy.CURRENT_COLOR_INDEX += 1
+            print(self.candies)
+            return ""
+        else:
+            print(self.candies)
+            return "Candy dispenser is full!"
+
+    def pop_candy(self) -> str:
         if not self.candies.is_empty():
             self.spring.grow()
 
-            self.candies.pop()
+            popped_candy = self.candies.pop()
             for i in range(len(self.candies)):
                 if i == 0:
                     self.candies[i].rect.midbottom = self.spring.rect.midtop
@@ -107,10 +122,27 @@ class CandyDispenser:
                     self.candies[i].rect.midbottom = self.candies[i - 1].rect.midtop
 
             Candy.CURRENT_COLOR_INDEX -= 1
+            print(self.candies)
+            return str(popped_candy)
+        else:
+            print(self.candies)
+            return "Candy dispenser is empty!"
 
-        print(self.candies)
+    def peek(self) -> str:
+        if not self.candies.is_empty():
+            return str(self.candies.peek())
+        else:
+            return "Candy dispenser is empty!"
+
+    def len(self) -> str:
+        return str(len(self.candies))
+
+    def is_empty(self) -> str:
+        return str(self.candies.is_empty())
 
     def render(self, surface: Surface):
+        pg.draw.rect(surface, "White", self.container_rect)
+        pg.draw.rect(surface, "Darkgrey", self.container_rect, 10)
         self.spring.render(surface)
 
         for candy in self.candies:
